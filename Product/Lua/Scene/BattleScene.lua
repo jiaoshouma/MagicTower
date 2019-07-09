7535a70f71909ca9a08873024e8d53059391f515
jiaoshouma <774818761@qq.com>
2019-07-10 04:20:27 +0800

    Expand battle Scene.Make UI fit root.

--- Product/Lua/Scene/BattleScene.lua
+++ Product/Lua/Scene/BattleScene.lua
@@ -3,6 +3,7 @@ local BattleScene = class("BattleScene")
 function BattleScene:ctor(...)
 	self.rootGo_ = GameObject.FindWithTag("BattleRoot")
 	self.battleCanvasGo_ = self.rootGo_.transform:Find("battle_canvas").gameObject
+	self.battleRootGo_ = self.rootGo_.transform:Find("battle_root").gameObject
 	self.eventProxy_ = sun.EventDispatcher.inner():newProxy("BattleScene")
 	self:registerEvents()
 	self:initScene()
@@ -17,8 +18,10 @@ end
 -- end
 
 function BattleScene:initScene()
-	local card = sun.CardManager.get():createCard(7,sun.CardType.ROLE)
-	SunUtils.AddChild(self.battleCanvasGo_,card.go_)
+	local battlefield = sun.AssetsLoader.get():loadAsset("Prefabs/Battle/battlefield.prefab")
+	SunUtils.AddChild(self.battleRootGo_,battlefield)
+	-- local card = sun.CardManager.get():createCard(7,sun.CardType.ROLE)
+	-- SunUtils.AddChild(self.battleCanvasGo_,card.go_)
 end
 
 return BattleScene
